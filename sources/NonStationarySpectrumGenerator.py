0e9ecc3b0562cd1126c48c99bd5b1393d7976eb8
bfundaro <bastien.fundaro@laposte.net>
2017-02-14 11:04:02 +0100

    NonStationarySpectrumGenerator : Correction des erreurs

--- sources/NonStationarySpectrumGenerator.py
+++ sources/NonStationarySpectrumGenerator.py
@@ -1,14 +1,15 @@
 from SpectrumGenerator import *
 
-class NonStationarySpectrumGenerator(Exception)
+class NonStationarySpectrumGeneratorError(Exception)
     """Base class for exception regarding the spectrum class"""
     pass
 
-class NonStationarySpectrumGenerator:
+class NonStationarySpectrumGenerator(SpectrumGenerator):
 
-    def _init_(self,_regular_lut,_lut):
+    def _init_(self,_regular_lut,_lut,_window_size, parameters, spectrum):
         self._regular_lut = _regular_lut
         self._lut = _lut
+        SpectrumGenerator.__init__(self,_window_size, parameters, spectrum)
 
     @classmethod
     def _add_lobe(self,k):
