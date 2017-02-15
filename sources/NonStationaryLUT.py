7c7ae42d3c703e9d2f37345dbe1c9a29b325ab65
ClémentMaliet <clement.maliet@orange.fr>
2017-02-15 16:30:08 +0100

    Non Stationary LUT dans les chou

--- sources/NonStationaryLUT.py
+++ sources/NonStationaryLUT.py
@@ -4,13 +4,14 @@
 # Stationary and non-stationary sinusoidal model synthesis with phase vocoder and FFT-1
 # Clément CAZORLA, Vincent CHRUN, Bastien FUNDARO, Clément MALIET
 
-import numpy as np
-from scipy import signal, interp1d
+from scipy import signal
+from scipy.interpolate import interp1d
 import warnings
+from data_structure import *
 
 
 def next2pow(x):
-    return 2**int(N.ceil(N.log(float(x))/N.log(2.0)))
+    return 2**int(np.ceil(np.log(float(x))/np.log(2.0)))
 
 
 class NonStationaryLUT(object):
@@ -60,63 +61,62 @@ class NonStationaryLUT(object):
         t = (1/self._fs)*n
         zero_padding_factor = 9
         nfft = 2**(next2pow(self._window_size)+zero_padding_factor)
-        f = np.array(range(nfft))*(self._fs/float(nfft))
 
         for i in xrange(self._number_acr):
             for j in xrange(self._number_fcr):
-                # Generate chirp
-                mu = acr[i]
-                psi = fcr[j]
-                s = (1 + mu*t)*np.exp(1j*((0.5*psi*t**2)+(2*np.pi*0.0227*n)))
-
-                # Apply window
-                s *= self._window
-
-                # Correct phase
-                sw = np.zeros(nfft)
-                sw[:(self._window_size-1)/2] = s[(self._window_size-1)/2:]
-                sw[nfft-(self._window_size-1)/2:] = s[:(self._window_size+1)/2]
-
-                # Compute the fft
-                fft_s = np.fft.fft(sw, nfft)
-                mod_fft_s = np.absolute(fft_s)
-                arg_fft_s = 0
-
-                # Search for the main lobe
-                mod_max = np.amax(mod_fft_s)
-                index = np.argmax(mod_fft_s)
-                argmax = f[index]
-
-                # Find a more accurate value of the main lobe peak
-                interp_index = self._quad_argmax(index, mod_fft_s[index-1], mod_fft_s[index], mod_fft_s[index+1])
-                interp_max = self._quad_max(mod_fft_s[index-1], mod_fft_s[index], mod_fft_s[index+1])
-                f_interp = interp1d(range(nfft), f)
-                interp_argmax = f_interp(interp_index)
-
-                # Initialize tests to find the zeros
-                left_peak = mod_fft_s[:-2] > mod_fft_s[1:-1]
-                right_peak = mod_fft_s[2:] > mod_fft_s[1:-1]
-
-                # Find location of zeros in the FFT
-                mod_zeros_loc = np.nonzero(np.logical_and(left_peak, right_peak))
-
-                # Find the closest zero to the lob peak
-                index_min = np.argmin(np.absolute(index - mod_zeros_loc))
-
-                # Set the lower and upper zero of the lobe
-                if mod_zeros_loc[index_min] > index:
-                    lower_zero_loc = mod_zeros_loc[index_min-1]
-                    upper_zero_loc = mod_zeros_loc[index_min]
-                else:
-                    lower_zero_loc = mod_zeros_loc[index_min]
-                    upper_zero_loc = mod_zeros_loc[index_min+1]
-
-                # Indexes of the main lobe
-                lobe_index = np.array(range(lower_zero_loc, upper_zero_loc))
-
-                # Split the lobe in 9 lobe points
-                x = np.linspace(lower_zero_loc*1.001, upper_zero_loc*0.999, 9)
-                x_phase = np.linspace(lower_zero_loc, upper_zero_loc, 1000)
+                pass
 
     def _gen_non_uniform_lut(self):
         pass
+
+    def _gen_lobe_legacy(self, i, j, acr, fcr, t, n, nfft):
+        # Generate chirp
+        mu = acr[i]
+        psi = fcr[j]
+        s = (1 + mu * t) * np.exp(1j * ((0.5 * psi * t ** 2) + (2 * np.pi * 0.0227 * n)))
+
+        # Apply window
+        s *= self._window
+
+        # Correct phase
+        sw = np.zeros(nfft)
+        sw[:(self._window_size - 1) / 2] = s[(self._window_size - 1) / 2:]
+        sw[nfft - (self._window_size - 1) / 2:] = s[:(self._window_size + 1) / 2]
+
+        # Compute the fft
+        fft_s = np.fft.fft(sw, nfft)
+        mod_fft_s = np.absolute(fft_s)
+
+        # Search for the main lobe
+        index = np.argmax(mod_fft_s)
+
+        # Initialize tests to find the zeros
+        left_peak = mod_fft_s[:-2] > mod_fft_s[1:-1]
+        right_peak = mod_fft_s[2:] > mod_fft_s[1:-1]
+
+        # Find location of zeros in the FFT
+        mod_zeros_loc = np.nonzero(np.logical_and(left_peak, right_peak))[0]
+
+        # Find the closest zero to the lob peak
+        index_min = np.argmin(np.absolute(index - mod_zeros_loc))
+
+        # Set the lower and upper zero of the lobe
+        if mod_zeros_loc[index_min] > index:
+            lower_zero_loc = mod_zeros_loc[index_min - 1]
+            upper_zero_loc = mod_zeros_loc[index_min]
+        else:
+            lower_zero_loc = mod_zeros_loc[index_min]
+            upper_zero_loc = mod_zeros_loc[index_min + 1]
+
+        # Indexes of the main lobe
+        lobe_index = np.array(range(lower_zero_loc, upper_zero_loc))
+
+        # Split the lobe in 9 lobe points
+        x = np.linspace(lower_zero_loc * 1.001, upper_zero_loc * 0.999, 9)
+
+        # Store the relevant lobe
+        f_interp = interp1d(range(nfft), mod_fft_s)
+        mag_lobe = f_interp(x)
+        f_interp = interp1d(lobe_index, np.unwrap(np.angle(fft_s[lobe_index])))
+        arg_lobe = f_interp(x)
+        x_lobe = ((x - 1) / nfft) - 0.0227  # f0/fs = 0.0227
