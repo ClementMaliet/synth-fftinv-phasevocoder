18d7f7f0db6e81716813c24424c4a6a450be9e4c
VinceChr <vincent.chrun@gmail.com>
2017-02-15 16:47:52 +0100

    A code to test different classes
    Here is the phasevocoding test. Not very useful for now, but we can see
    it kinda works at first glance :dancer:

---
+++ examples/main_test_PhaseVocoder.py
@@ -0,0 +1,27 @@
+from sources.SpectrumGenerator import SpectrumGenerator
+from sources.StationaryLobe import *
+from sources.PhaseVocoder import *
+from scipy import signal
+import matplotlib.pyplot as plt
+
+from sources.StationarySpectrumGenerator import StationarySpectrumGenerator
+
+window_size = 1024
+window_type = 'hamming'
+
+s = StationaryLobe(window_type, window_size)
+
+parameters = Parameters(np.array([2]), np.array([0.3]), np.array([50]))
+
+sg = StationarySpectrumGenerator(window_type, window_size, parameters)
+
+pv_stat_test = StationaryPhaseVocoder(1, 4, sg.get_spectrum()) #(Analyse_hop, Synthesis_hop, Current_analysis_spectrum)
+
+#Synthetic Spectrum
+plt.figure()
+plt.plot(np.linspace(0, 1, window_size), sg.get_spectrum()._amplitude)
+
+#Phase Vocoded Spectrum
+plt.figure()
+plt.plot(np.linspace(0, 1, window_size), pv_stat_test.get_pv_spectrum()._amplitude)
+plt.show()
