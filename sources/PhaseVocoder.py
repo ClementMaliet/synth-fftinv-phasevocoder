633803d1845f836acc10930fa765c87001bbfca0
VinceChr <vincent.chrun@gmail.com>
2017-02-15 14:38:55 +0100

    Phase vocoder finished ? 0.3

--- sources/PhaseVocoder.py
+++ sources/PhaseVocoder.py
@@ -5,13 +5,15 @@
 # Clément CAZORLA, Vincent CHRUN, Bastien FUNDARO, Clément MALIET
 
 # Thi file contains the definition of the Phase vocoder class
-import numpy as np
+
 from abc import ABCMeta, abstractmethod
 from data_structure import *
+
 
 class PhaseVocoderError(Exception):
     """Base class for exception regarding the spectrum class"""
     pass
+
 
 class InconsistentHopSizeError(PhaseVocoderError):
     """Raised if the hop size is larger than the nfft size of given spectrum"""
@@ -21,14 +23,16 @@ class InconsistentHopSizeError(PhaseVocoderError):
 
 class PhaseVocoder(object):
     __metaclass__ = ABCMeta
-    """The abstract class PhaseVocoder is used to get a phase-vocoded spectrum."""
+    """The abstract class PhaseVocoder is used to get a phase-vocoder spectrum."""
     def __init__(self, analysis_hop, synthesis_hop, current_analysis_spectrum):
-        if analysis_hop >= current_analysis_spectrum.get_nfft() or synthesis_hop >= current_analysis_spectrum.get_nfft():
+        if analysis_hop >= current_analysis_spectrum.get_nfft() or \
+                synthesis_hop >= current_analysis_spectrum.get_nfft():
             raise InconsistentHopSizeError('Synthesis and/or Analysis Hop provided have inconsistent size')
         else:
             self._analysis_hop = analysis_hop
             self._synthesis_hop = synthesis_hop
-            self._omega = np.array(range(current_analysis_spectrum.get_nfft()))*((2*np.pi)/current_analysis_spectrum.get_nfft())
+            self._omega = np.array(range(current_analysis_spectrum.get_nfft())) *\
+                ((2*np.pi)/current_analysis_spectrum.get_nfft())
             self._past_analysis_spectrum = Spectrum.void_spectrum(current_analysis_spectrum.get_nfft())
             self._past_synthesis_spectrum = Spectrum.void_spectrum(current_analysis_spectrum.get_nfft())
             self._current_analysis_spectrum = current_analysis_spectrum
@@ -37,7 +41,7 @@ class PhaseVocoder(object):
     def _set_current_analysis_spectrum(self, new_spectrum):
         self._past_analysis_spectrum = self._current_analysis_spectrum
         self._past_synthesis_spectrum = self.current_synthesis_spectrum
-        # current synth a zero
+        self.current_synthesis_spectrum = Spectrum.void_spectrum(self._current_analysis_spectrum.get_nfft())
         self._current_analysis_spectrum = new_spectrum
     current_analysis_spectrum = property(fset=_set_current_analysis_spectrum)
 
@@ -52,27 +56,23 @@ class PhaseVocoder(object):
 
 # Stationary Phase Vocoder
 
+
 class StationaryPhaseVocoder(PhaseVocoder):
 
-
-
-    def get_pv_spectrum(self,k):
+    def get_pv_spectrum(self):
         """Phase vocoder algorithm"""
-        amplitude = _current_analysis_spectrum.get_amplitude(k)
-        phase = _current_analysis_spectrum.get_phase(k)
-        previous_phase = _past_analysis_spectrum.get_phase(k)
-        win_size = len(_past_analysis_spectrum.get_nfft(k))
 
-        delta_phi = phase - previous_phase
-        delta_phi_prime = delta_phi - self._analysis_hop * 2 * np.pi * np.array[0:(win_size - 1)]/win_size
-        delta_phi_prime_mod = (delta_phi_prime + np.pi) % (2 * np.pi) - np.pi
-        true_freq = 2 * np.pi * np.array[0:(win_size - 1)] / win_size + delta_phi_prime_mod / self._analysis_hop
+        for k in xrange(self.current_synthesis_spectrum.get_nfft()):
+            amplitude = self._current_analysis_spectrum.get_amplitude(k)
+            phase = self._current_analysis_spectrum.get_phase(k)
+            previous_phase = self._past_analysis_spectrum.get_phase(k)
+            win_size = self._past_analysis_spectrum.get_nfft()
 
+            delta_phi = phase - previous_phase
+            delta_phi_prime = delta_phi - self._analysis_hop * 2 * np.pi * np.array[0:(win_size - 1)]/win_size
+            delta_phi_prime_mod = (delta_phi_prime + np.pi) % (2 * np.pi) - np.pi
+            true_freq = 2 * np.pi * np.array[0:(win_size - 1)] / win_size + delta_phi_prime_mod / self._analysis_hop
 
-        current_synthesis_spectrum *= phase + self._synthesis_hop * true_freq
-
-        return current_synthesis_spectrum
-
-
-
-
+            self.current_synthesis_spectrum += amplitude*np.exp(1j*(phase + self._synthesis_hop * true_freq)) *\
+                self._kronecker_array(k)
+        return self.current_synthesis_spectrum
