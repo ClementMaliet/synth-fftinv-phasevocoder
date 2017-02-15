e5c0f3a2698dcffc5682a654058a60e0925020c0
ClémentMaliet <clement.maliet@orange.fr>
2017-02-15 14:27:17 +0100

    Various corrections Phase Vocoder
    __init__ corrections
    _kronecker_array added

--- sources/PhaseVocoder.py
+++ sources/PhaseVocoder.py
@@ -22,29 +22,33 @@ class InconsistentHopSizeError(PhaseVocoderError):
 class PhaseVocoder(object):
     __metaclass__ = ABCMeta
     """The abstract class PhaseVocoder is used to get a phase-vocoded spectrum."""
-    def __init__(self, analysis_hop, synthesis_hop, past_analysis_spectrum, past_synthesis_spectrum, current_analysis_spectrum, current_synthesis_spectrum):
-        if len(analysis_hop) >= len(past_analysis_spectrum.get_nfft()) or len(synthesis_hop) >= len(past_synthesis_spectrum.get_nfft()):
+    def __init__(self, analysis_hop, synthesis_hop, current_analysis_spectrum):
+        if analysis_hop >= current_analysis_spectrum.get_nfft() or synthesis_hop >= current_analysis_spectrum.get_nfft():
             raise InconsistentHopSizeError('Synthesis and/or Analysis Hop provided have inconsistent size')
         else:
             self._analysis_hop = analysis_hop
             self._synthesis_hop = synthesis_hop
-            self._omega = omega
-            self._past_analysis_spectrum = past_analysis_spectrum
-            self._past_synthesis_spectrum = past_synthesis_spectrum
+            self._omega = np.array(range(current_analysis_spectrum.get_nfft()))*((2*np.pi)/current_analysis_spectrum.get_nfft())
+            self._past_analysis_spectrum = Spectrum.void_spectrum(current_analysis_spectrum.get_nfft())
+            self._past_synthesis_spectrum = Spectrum.void_spectrum(current_analysis_spectrum.get_nfft())
             self._current_analysis_spectrum = current_analysis_spectrum
-            self.current_synthesis_spectrum = current_synthesis_spectrum
+            self.current_synthesis_spectrum = Spectrum.void_spectrum(current_analysis_spectrum.get_nfft())
 
     def _set_current_analysis_spectrum(self, new_spectrum):
         self._past_analysis_spectrum = self._current_analysis_spectrum
-        self._past_synthesis_spectrum = self._current_synthesis_spectrum
+        self._past_synthesis_spectrum = self.current_synthesis_spectrum
         # current synth a zero
         self._current_analysis_spectrum = new_spectrum
-    current_analysis_spectrum = property(_set_current_analysis_spectrum)
+    current_analysis_spectrum = property(fset=_set_current_analysis_spectrum)
 
     @abstractmethod
     def get_pv_spectrum(self):
         pass
 
+    def _kronecker_array(self, k):
+        ek = np.zeros(self._current_analysis_spectrum.get_nfft())
+        ek[k] = 1
+        return ek
 
 # Stationary Phase Vocoder
 
