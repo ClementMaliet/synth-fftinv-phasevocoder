9820220dc1c22ab5e8a36e6d1bd0241f0b4bacf2
ClémentMaliet <clement.maliet@orange.fr>
2017-02-20 16:03:14 +0100

    Negative frequencies correction

--- examples/main_stationnary_synthesis.py
+++ examples/main_stationnary_synthesis.py
@@ -4,24 +4,27 @@ import matplotlib.pyplot as plt
 
 window_size = 1025
 window_type = "hamming"
-zero_padding_factor = 3
+zero_padding_factor = 1
 nfft = 2**(next2pow(1025) + zero_padding_factor)
 
-parameter = Parameters(np.array([1]), np.array([0.12]), np.array([3.]))
+parameter = Parameters(np.array([1, 3]), np.array([0.12, 0.29]), np.array([3., 0.2]))
 synth = StationarySynthesizer(window_size, window_type, zero_padding_factor, 500, 500, parameter)
 synth.set_next_frame(parameter)
 s = synth.get_next_frame()
 w = signal.get_window(window_type, window_size)
 w /= np.sum(w)
-s_gt = parameter.get_amplitude(0)*np.exp(2*np.pi*1j*parameter.get_frequency(0)*np.array(range(1025)) +
-                                         parameter.get_phase(0)*1j).real
+s_gt = np.zeros(window_size)
+for i in xrange(parameter.get_number_sinuses()):
+    s_gt += parameter.get_amplitude(i)*np.exp(2*np.pi*1j*parameter.get_frequency(i)*np.array(range(window_size)) +
+                                             parameter.get_phase(i)*1j).real
+
 s_gt *= w
 
 sw_gt = np.zeros(nfft)
 sw_gt[:(window_size - 1) / 2] = s_gt[((window_size + 1) / 2):]
 sw_gt[nfft - (window_size - 1) / 2 - 1:] = s_gt[:(window_size + 1) / 2]
 
-omega = np.arange(nfft) * nfft
+omega = np.arange(nfft) / float(nfft)
 
 sfft = np.fft.fft(sw_gt, nfft)
 
